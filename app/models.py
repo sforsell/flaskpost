ed56402ded558e34faba10e82edbf1f6f9d8675f
sforsell <mskforsell@gmail.com>
2019-04-23 17:06:42 -0400

    add posts model + migrations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,5 +7,17 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    posts = db.relationship('Post', backref='author', lazy='dynamic')  # for relationship reference model -> CamelCase
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # for ForeignKey reference table name -> snake_case
+
+    def __repr__(self):
+        return '<Post {}>'.format((self.body))
