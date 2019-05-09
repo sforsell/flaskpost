f65a7edb7bc7cb8f12dacc37d9fa31afa98ea290
sforsell <mskforsell@gmail.com>
2019-05-09 15:36:39 -0400

    add logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
@@ -30,6 +31,13 @@ if not app.debug:
                                    secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flaskpost.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.info('FlaskPost startup')
 
 from app import routes, models, errors
 
