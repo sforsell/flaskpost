12024a9250fcc84f26bbae0235351eab47a68af5
sforsell <mskforsell@gmail.com>
2019-05-28 15:33:40 -0400

    add discover page and made tweaks to posts thumbnail and index to accomodate

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,7 @@ def index():
         flash('Your post is now live :)')
         return redirect(url_for('index'))
     posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home Page', posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
@@ -67,10 +67,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = user.posts
     return render_template('user.html', user=user, posts=posts)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -117,3 +114,9 @@ def unfollow(username):
     db.session.commit()
     flash('You are no longer following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+@app.route('/discover')
+@login_required
+def discover():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Discover', posts=posts)
