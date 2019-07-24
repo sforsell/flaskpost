675ed5e67bb6468c104d0f8c18c388783c6e794a
sforsell <mskforsell@gmail.com>
2019-07-24 16:50:47 -0400

    add bootstrap and update base.html. rename clock content to app_content due to bootstrap naming conventions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
